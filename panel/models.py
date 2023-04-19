9a6a9e320f8c5750d0ccc59cca1a67bbb7f75791
gonzalesluisfrancisco <gonzales.luisf@gmail.com>
2023-04-19 10:06:16 -0500

    Cambio de tipo de modelos

--- panel/models.py
+++ panel/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class PersonalRegistrado(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     cardidHex = models.CharField(max_length=8, null=False)
     nombre = models.CharField(max_length=50, null=False)
     apellido = models.CharField(max_length=50, null=False)
